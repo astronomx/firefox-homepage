08b7a6502e5ea1b863b9227ccc08f8bf260219dd
astronomx <quentinvalen@gmail.com>
2023-11-15 14:06:36 +0100

    Styled clock and made it mobile friendly

---
+++ src/app/components/Clock.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+
+export default function Clock() {
+    const [time, setTime] = useState<string>();
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString("nl-NL", { hour: '2-digit', minute: '2-digit' }));
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    if (time === null) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h1 className="text-[5rem]">{time}</h1>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-import React from "react"
-
-import SearchBar from "./components/SearchBar"
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import Clock from "./components/Clock";
 
 export default function Home() {
-
-
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen" style={{ backgroundImage: "url('https://wallpaperaccess.com/full/8351171.gif')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-        
+      <div
+        className="flex min-h-screen justify-center items-center"
+        style={{
+          backgroundImage: "url('https://wallpaperaccess.com/full/8351171.gif')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex justify-center items-center md: p-4 absolute top-0 mt-36">
+          <Clock />
+        </div>
+
         <div>
-          <div className="flex">
-            <SearchBar />
-          </div>
+          <SearchBar />
         </div>
       </div>
     </>
-  )
+  );
 }
-
