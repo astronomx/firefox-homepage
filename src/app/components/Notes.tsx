a11b3ef5dfdcfc8863f48b6f67eddf54d8845b43
astronomx <unkownusernotfound@protonmail.com>
2023-11-27 11:55:25 +0100

    Mobile friendly

--- src/app/components/Notes.tsx
+++ src/app/components/Notes.tsx
@@ -50,47 +50,49 @@ export default function Notes() {
 
   return (
     <>
-      <div>
-        <h1 className="text-4xl font-bold">Notes</h1>
-        <ul>
-          {notes.map((note, index) => (
-            <div key={index} className="mb-3">
-              <li className="flex flex-col text-lg bg-[#FF9EFF] p-2 rounded-lg w-80 break-all">
-                <span>{formatTimestamp(note.timestamp)}</span>
-
-                <div className="flex items-center">
-                  <span>{note.text}</span>
-
-                  <button onClick={() => deleteNote(index)}>
-                    <TrashIcon className="h-4 w-4 text-white" />
-                  </button>
-                </div>
-              </li>
+      <div className="hidden lg:block">
+        <div>
+          <h1 className="text-4xl font-bold">Notes</h1>
+          <ul>
+            {notes.map((note, index) => (
+              <div key={index} className="mb-3 drop-shadow-lg">
+                <li className="flex flex-col text-lg bg-[#FF9EFF] p-2 rounded-lg w-80 break-all relative">
+                  <span>{formatTimestamp(note.timestamp)}</span>
+            
+                  <div className="flex items-center justify-between">
+                    <span>{note.text}</span>
+            
+                    <button onClick={() => deleteNote(index)} className="absolute right-2 top-2">
+                      <TrashIcon className="h-4 w-4 text-white" />
+                    </button>
+                  </div>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </div>
+        <div>
+          {isAddingNote ? (
+            <div className="flex flex-col space-y-4 mt-5">
+              <input
+                type="text"
+                value={newNote}
+                onChange={(e) => setNewNote(e.target.value)}
+                onKeyDown={handleKeyDown}
+                className="w-80 h-10 rounded-lg drop-shadow-xl text-black text-md px-4 break-all"
+              />
+              <div className="flex justify-center">
+                <button className="bg-[#FF9EFF] px-4 py-1 rounded-lg" onClick={() => setIsAddingNote(false)}>
+                  Cancel
+                </button>
+              </div>
             </div>
-          ))}
-        </ul>
-      </div>
-      <div>
-        {isAddingNote ? (
-          <div className="flex flex-col space-y-4 mt-5">
-            <input
-              type="text"
-              value={newNote}
-              onChange={(e) => setNewNote(e.target.value)}
-              onKeyDown={handleKeyDown}
-              className="w-80 h-10 rounded-lg drop-shadow-xl text-black text-md px-4 break-all"
-            />
-            <div className="flex justify-center">
-              <button className="bg-[#FF9EFF] px-4 py-1 rounded-lg" onClick={() => setIsAddingNote(false)}>
-                Cancel
-              </button>
-            </div>
-          </div>
-        ) : (
-          <button className="flex justify-center items-center w-80 h-10 bg-[#8300C4] rounded-lg drop-shadow-xl mt-5" onClick={() => setIsAddingNote(true)}>
-            <PlusCircleIcon className="h-6 w-6 text-white" />
-          </button>
-        )}
+          ) : (
+            <button className="flex justify-center items-center w-80 h-10 bg-[#8300C4] rounded-lg drop-shadow-xl mt-5" onClick={() => setIsAddingNote(true)}>
+              <PlusCircleIcon className="h-6 w-6 text-white" />
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
